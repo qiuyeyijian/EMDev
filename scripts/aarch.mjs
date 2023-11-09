e83b2ff80c34d7009f95adfa7814c20e5341de43
qiuyeyijian <zlw9821@qq.com>
2023-11-09 23:25:41 +0800

    chore: 更新一些文件

--- scripts/aarch.mjs
+++ scripts/aarch.mjs
@@ -2,99 +2,93 @@
  * Build and upload assets
  * for macOS(aarch)
  */
-import fs from "fs-extra";
-import path from "path";
-import { exit } from "process";
-import { execSync } from "child_process";
-import { createRequire } from "module";
-import { getOctokit, context } from "@actions/github";
+import fs from "fs-extra"
+import path from "path"
+import { exit } from "process"
+import { execSync } from "child_process"
+import { createRequire } from "module"
+import { getOctokit, context } from "@actions/github"
 
 // to `meta` tag
-const META = process.argv.includes("--meta");
+const META = process.argv.includes("--meta")
 // to `alpha` tag
-const ALPHA = process.argv.includes("--alpha");
+const ALPHA = process.argv.includes("--alpha")
 
-const require = createRequire(import.meta.url);
+const require = createRequire(import.meta.url)
 
 async function resolve() {
   if (!process.env.GITHUB_TOKEN) {
-    throw new Error("GITHUB_TOKEN is required");
+    throw new Error("GITHUB_TOKEN is required")
   }
   if (!process.env.GITHUB_REPOSITORY) {
-    throw new Error("GITHUB_REPOSITORY is required");
+    throw new Error("GITHUB_REPOSITORY is required")
   }
   if (!process.env.TAURI_PRIVATE_KEY) {
-    throw new Error("TAURI_PRIVATE_KEY is required");
+    throw new Error("TAURI_PRIVATE_KEY is required")
   }
   if (!process.env.TAURI_KEY_PASSWORD) {
-    throw new Error("TAURI_KEY_PASSWORD is required");
+    throw new Error("TAURI_KEY_PASSWORD is required")
   }
 
-  const { version } = require("../package.json");
+  const { version } = require("../package.json")
 
-  const tag = META ? "meta" : ALPHA ? "alpha" : `v${version}`;
-  const buildCmd = META ? `yarn build -f default-meta` : `yarn build`;
+  const tag = META ? "meta" : ALPHA ? "alpha" : `v${version}`
+  const buildCmd = META ? `yarn build -f default-meta` : `yarn build`
 
-  console.log(`[INFO]: Upload to tag "${tag}"`);
-  console.log(`[INFO]: Building app. "${buildCmd}"`);
+  console.log(`[INFO]: Upload to tag "${tag}"`)
+  console.log(`[INFO]: Building app. "${buildCmd}"`)
 
-  execSync(buildCmd);
+  execSync(buildCmd)
 
-  const cwd = process.cwd();
-  const bundlePath = path.join(cwd, "src-tauri/target/release/bundle");
-  const join = (p) => path.join(bundlePath, p);
+  const cwd = process.cwd()
+  const bundlePath = path.join(cwd, "src-tauri/target/release/bundle")
+  const join = (p) => path.join(bundlePath, p)
 
-  const appPathList = [
-    join("macos/Clash Verge.aarch64.app.tar.gz"),
-    join("macos/Clash Verge.aarch64.app.tar.gz.sig"),
-  ];
+  const appPathList = [join("macos/Clash Verge.aarch64.app.tar.gz"), join("macos/Clash Verge.aarch64.app.tar.gz.sig")]
 
   for (const appPath of appPathList) {
     if (fs.pathExistsSync(appPath)) {
-      fs.removeSync(appPath);
+      fs.removeSync(appPath)
     }
   }
 
-  fs.copyFileSync(join("macos/Clash Verge.app.tar.gz"), appPathList[0]);
-  fs.copyFileSync(join("macos/Clash Verge.app.tar.gz.sig"), appPathList[1]);
+  fs.copyFileSync(join("macos/Clash Verge.app.tar.gz"), appPathList[0])
+  fs.copyFileSync(join("macos/Clash Verge.app.tar.gz.sig"), appPathList[1])
 
-  const options = { owner: context.repo.owner, repo: context.repo.repo };
-  const github = getOctokit(process.env.GITHUB_TOKEN);
+  const options = { owner: context.repo.owner, repo: context.repo.repo }
+  const github = getOctokit(process.env.GITHUB_TOKEN)
 
   const { data: release } = await github.rest.repos.getReleaseByTag({
     ...options,
-    tag,
-  });
+    tag
+  })
 
-  if (!release.id) throw new Error("failed to find the release");
+  if (!release.id) throw new Error("failed to find the release")
 
-  await uploadAssets(release.id, [
-    join(`dmg/Clash Verge_${version}_aarch64.dmg`),
-    ...appPathList,
-  ]);
+  await uploadAssets(release.id, [join(`dmg/Clash Verge_${version}_aarch64.dmg`), ...appPathList])
 }
 
 // From tauri-apps/tauri-action
 // https://github.com/tauri-apps/tauri-action/blob/dev/packages/action/src/upload-release-assets.ts
 async function uploadAssets(releaseId, assets) {
-  const github = getOctokit(process.env.GITHUB_TOKEN);
+  const github = getOctokit(process.env.GITHUB_TOKEN)
 
   // Determine content-length for header to upload asset
-  const contentLength = (filePath) => fs.statSync(filePath).size;
+  const contentLength = (filePath) => fs.statSync(filePath).size
 
   for (const assetPath of assets) {
     const headers = {
       "content-type": "application/zip",
-      "content-length": contentLength(assetPath),
-    };
+      "content-length": contentLength(assetPath)
+    }
 
-    const ext = path.extname(assetPath);
-    const filename = path.basename(assetPath).replace(ext, "");
+    const ext = path.extname(assetPath)
+    const filename = path.basename(assetPath).replace(ext, "")
     const assetName = path.dirname(assetPath).includes(`target${path.sep}debug`)
       ? `${filename}-debug${ext}`
-      : `${filename}${ext}`;
+      : `${filename}${ext}`
 
-    console.log(`[INFO]: Uploading ${assetName}...`);
+    console.log(`[INFO]: Uploading ${assetName}...`)
 
     try {
       await github.rest.repos.uploadReleaseAsset({
@@ -103,17 +97,17 @@ async function uploadAssets(releaseId, assets) {
         data: fs.readFileSync(assetPath),
         owner: context.repo.owner,
         repo: context.repo.repo,
-        release_id: releaseId,
-      });
+        release_id: releaseId
+      })
     } catch (error) {
-      console.log(error.message);
+      console.log(error.message)
     }
   }
 }
 
 if (process.platform === "darwin" && process.arch === "arm64") {
-  resolve();
+  resolve()
 } else {
-  console.error("invalid");
-  exit(1);
+  console.error("invalid")
+  exit(1)
 }
--- scripts/updatelog.mjs
+++ scripts/updatelog.mjs
@@ -1,44 +1,44 @@
-import fs from "fs-extra";
-import path from "path";
+import fs from "fs-extra"
+import path from "path"
 
-const UPDATE_LOG = "UPDATELOG.md";
+const UPDATE_LOG = "UPDATELOG.md"
 
 // parse the UPDATELOG.md
 export async function resolveUpdateLog(tag) {
-  const cwd = process.cwd();
+  const cwd = process.cwd()
+  /* eslint-disable-next-line */
+  const reTitle = /^## v[\d\.]+/
+  const reEnd = /^---/
 
-  const reTitle = /^## v[\d\.]+/;
-  const reEnd = /^---/;
-
-  const file = path.join(cwd, UPDATE_LOG);
+  const file = path.join(cwd, UPDATE_LOG)
 
   if (!(await fs.pathExists(file))) {
-    throw new Error("could not found UPDATELOG.md");
+    throw new Error("could not found UPDATELOG.md")
   }
 
-  const data = await fs.readFile(file).then((d) => d.toString("utf8"));
+  const data = await fs.readFile(file).then((d) => d.toString("utf8"))
 
-  const map = {};
-  let p = "";
+  const map = {}
+  let p = ""
 
   data.split("\n").forEach((line) => {
     if (reTitle.test(line)) {
-      p = line.slice(3).trim();
+      p = line.slice(3).trim()
       if (!map[p]) {
-        map[p] = [];
+        map[p] = []
       } else {
-        throw new Error(`Tag ${p} dup`);
+        throw new Error(`Tag ${p} dup`)
       }
     } else if (reEnd.test(line)) {
-      p = "";
+      p = ""
     } else if (p) {
-      map[p].push(line);
+      map[p].push(line)
     }
-  });
+  })
 
   if (!map[tag]) {
-    throw new Error(`could not found "${tag}" in UPDATELOG.md`);
+    throw new Error(`could not found "${tag}" in UPDATELOG.md`)
   }
 
-  return map[tag].join("\n").trim();
+  return map[tag].join("\n").trim()
 }
