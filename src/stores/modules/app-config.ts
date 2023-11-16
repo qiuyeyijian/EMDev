8a2e2d68e19329560f0ed06944e0e6e94984ca28
qiuyeyijian <zlw9821@qq.com>
2023-11-16 22:51:14 +0800

    chore: 优化一些问题

--- src/stores/modules/app-config.ts
+++ src/stores/modules/app-config.ts
@@ -16,29 +16,16 @@ export const useAppConfigStore = defineStore("app-config", () => {
     console.log("app config init")
     const configPath = await path.join(await path.configDir(), "emdev/config.json")
     appConfig.value = new Store(configPath)
-    if (import.meta.env.VITE_RUN_MODE === "dev") {
-      appConfig.value.get("jenkins").then((res) => {
-        if (!res) {
-          appConfig.value.set("jenkins", {
-            baseUrl: "http://192.168.1.39:8080/",
-            username: "qiuyeyijian",
-            password: "113cef47db0668bf6b18c56519c91c3571",
-            token: "113cef47db0668bf6b18c56519c91c3571"
-          })
-        }
-      })
-    } else {
-      appConfig.value.get("jenkins").then((res) => {
-        if (!res) {
-          appConfig.value.set("jenkins", {
-            baseUrl: "http://10.10.7.23:8080/",
-            username: "longwei.zhang",
-            password: "11e96896b069851c967446523de2725171",
-            token: "11e96896b069851c967446523de2725171"
-          })
-        }
-      })
-    }
+    appConfig.value.get("jenkins").then((res) => {
+      if (!res) {
+        appConfig.value.set("jenkins", {
+          baseUrl: "http://10.10.7.23:8080/",
+          username: "longwei.zhang",
+          password: "11e96896b069851c967446523de2725171",
+          token: "11e96896b069851c967446523de2725171"
+        })
+      }
+    })
   }
 
   return { appConfig, init }
